cdd89ceee5575f16f8b5f2fb7b45be0ecac51b9f
Corentin Basler <corentin.basler@protonmail.ch>
2022-02-15 10:22:15 +0100

    Adding foclan_dyode_in & foclan_dyode_out

---
+++ dyodeV2/in/foclan_dyode_in_udp.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+import base64
+import pickle
+import struct
+import serial
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import protocol, reactor
+#---------------------------------------------------------------------------#
+
+# Logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+PORT_NUMBER = 3261
+
+def modbus_send_serial(data):
+    modbus_data = pickle.dumps(data)
+    data_length = len(modbus_data)
+    encoded_data = base64.b64encode(modbus_data)
+    data_size = sys.getsizeof(encoded_data)
+    log.debug('Data size : %s' % data_size)
+
+    ser = serial.Serial(
+        port='/dev/ttyAMA0',
+        baudrate = 57600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=0.5
+        )
+
+    ser.write(encoded_data)
+    ser.write('\n')
+    log.debug('written ' + str(len(encoded_data) + 1) + ' Bytes on serial')
+    
+class DyodeProtocol(DatagramProtocol):
+    def sendReply(self, data, address):
+        log.debug('send reply')
+        reply_content = data[0:4] + struct.pack('>H', 6) + data[6:12]
+        log.debug("send: " + " ".join([hex(ord(x)) for x in reply_content]))
+        self.transport.write(reply_content, address)
+
+    def datagramReceived(self, data, address):
+        log.debug('Received message')
+        log.debug("Received: " + " ".join([hex(ord(x)) for x in data]))
+        log.debug('function code')
+        log.debug(ord(data[7]))
+        log.debug('address')
+        log.debug(ord(data[9]))
+        log.debug(ord(data[10]))
+        modbus_send_serial(data)
+        self.sendReply(data, address)
+         
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+        
+if __name__ == '__main__':
+    reactor.listenUDP(PORT_NUMBER, DyodeProtocol())
+    reactor.run()
+    
+
+
---
+++ dyodeV2/out/foclan_dyode_out_udp.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Creation DT 2018 04 23
+# Last update DT 2018 05 03
+
+import logging
+import base64
+import pickle
+import serial
+import socket
+#---------------------------------------------------------------------------#
+
+# Logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+TARGET_IP = '10.48.159.255'
+TARGET_PORT = 3261
+
+
+def send_udp(data):
+    try:
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        udp_socket.sendto(data, (TARGET_IP, TARGET_PORT))
+        udp_socket.close()
+    except Exception, error:
+            log.debug('Connection failed to final destination')
+            log.debug(str(error))
+
+def get_modbus_data_serial():
+    ser = serial.Serial(
+        port='/dev/ttyAMA0',
+        baudrate = 115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+        )
+
+    serial_data=ser.readline()
+    serial_data = serial_data.rstrip()
+    if len(serial_data) > 0:
+        get_modbus_data_serial.counter += 1
+        # log.debug('packet n ' + str(get_modbus_data_serial.counter) + ', Bytes: ' + str(len(serial_data)))
+        decoded_data = base64.b64decode(serial_data)
+        depickeled_data = pickle.loads(decoded_data)
+        send_udp(depickeled_data)
+    ser.close()
+
+
+get_modbus_data_serial.counter = 0
+
+
+def receiving_loop():
+    log.debug('Modbus Loop')
+    while(1):
+        try:
+            get_modbus_data_serial()
+        except Exception, error:
+            log.debug('Error while updating modbus values')
+            log.debug(str(error))
+
+if __name__ == '__main__':
+    receiving_loop()
+
